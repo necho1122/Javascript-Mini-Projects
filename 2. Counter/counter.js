2180391e037584174bcf43d025bc03643825ee95
Nelson Ubac <nelsonubac90@gmail.com>
2023-11-10 13:50:18 -0300

    created counter

---
+++ 2. Counter/counter.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    let counter = 0;
+
+    const counterValue = document.querySelector('#counter');
+    const incrementDOM = document.querySelector('#btn-increment');
+    const decrementDOM = document.querySelector('#btn-decrement');
+
+    incrementDOM.addEventListener('click', increment=>{
+        counter++;
+        document.querySelector('#counter').innerHTML = counter;
+    })
+
+
+    decrementDOM.addEventListener('click', decrement=>{
+        counter--;
+        document.querySelector('#counter').innerHTML = counter;
+    })
+}
